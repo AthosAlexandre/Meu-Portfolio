1e1ab2a4f1f94fc9c3210734428f2cb53b6a79ff
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2024-01-11 09:42:15 -0400

    Colocando o componente de meus projetos

--- WEB/Portfolio/meu-portfolio/src/views/home/Home.vue
+++ WEB/Portfolio/meu-portfolio/src/views/home/Home.vue
@@ -2,9 +2,11 @@
   <InicioSobreMim />
   <MinhasSkills />
   <SobreMim />
+  <MeusProjetos />
 </template>
 <script>
 import InicioSobreMim from '@/components/inicio-sobre-mim/InicioSobreMim.vue'
+import MeusProjetos from '@/components/meus-projetos/MeusProjetos.vue';
 import MinhasSkills from '@/components/minhas-skills/MinhasSkills.vue'
 import SobreMim from '@/components/sobre-mim/SobreMim.vue';
  export default {
@@ -18,7 +20,8 @@ import SobreMim from '@/components/sobre-mim/SobreMim.vue';
    components: {
     InicioSobreMim,
     MinhasSkills,
-    SobreMim
+    SobreMim,
+    MeusProjetos
 },
  }
 </script>
