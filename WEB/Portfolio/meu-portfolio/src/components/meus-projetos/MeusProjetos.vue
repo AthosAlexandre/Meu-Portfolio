74cd4c6f7a96dfc6dde3958703a4edefb8a61b50
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2024-01-15 09:35:26 -0400

    coloquei um slider na minhas skills

--- WEB/Portfolio/meu-portfolio/src/components/meus-projetos/MeusProjetos.vue
+++ WEB/Portfolio/meu-portfolio/src/components/meus-projetos/MeusProjetos.vue
@@ -3,37 +3,58 @@
         <h2 class="font-weight-regular" style="text-align: center;">Meus Projetos</h2>
         <div class="container-cards-meus-projetos">
             <v-row>
-                <v-col>
+                <v-col v-for="(projeto, index) in projetos" :key="index" cols="12" sm="4">
                     <v-card class="mx-auto" max-width="344">
-                        <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
+                        <v-img :src="projeto.imagem" height="200px" cover></v-img>
 
                         <v-card-title>
-                            Top western road trips
+                            {{ projeto.titulo }}
                         </v-card-title>
 
                         <v-card-subtitle>
-                            1,000 miles of wonder
+                            {{ projeto.titulo }}
                         </v-card-subtitle>
 
+
                         <v-card-actions>
-                            <v-btn color="orange-lighten-2" variant="text">
-                                Explore
-                            </v-btn>
+                            <v-dialog width="500">
+                                <template v-slot:activator="{ props }">
+                                    <v-btn v-bind="props" color="black" prepend-icon="mdi-arrow-left-circle"
+                                        variant="elevated">
+                                        Ver Projeto
+                                    </v-btn>
+                                </template>
+
+                                <template v-slot:default="{ isActive }">
+                                    <v-card title="Quer ver a onde?">
+                                        <v-card-actions class="d-flex justify-space-evenly">
+                                           <a :href="projeto.repositorio" target="_blank"> <v-btn append-icon="mdi-github" style="width: 150px;" text="Repositório" color="black" variant="elevated"></v-btn></a>
+
+                                           <a :href="projeto.site" target="_blank"> <v-btn style="width: 150px;" text="Site" color="black" variant="elevated" append-icon="mdi-page-next"></v-btn></a>
+                                        </v-card-actions>
+
+                                        <v-card-actions>
+                                            <v-spacer></v-spacer>
+
+                                            <v-btn icon="mdi-close-box" @click="isActive.value = false"></v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </template>
+
+                            </v-dialog>
 
                             <v-spacer></v-spacer>
 
-                            <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
+                            <v-btn :icon="projeto.show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+                                @click="projeto.show = !projeto.show"></v-btn>
                         </v-card-actions>
 
                         <v-expand-transition>
-                            <div v-show="show">
+                            <div v-show="projeto.show">
                                 <v-divider></v-divider>
 
                                 <v-card-text>
-                                    I'm a thing. But, like most politicians, he promised more than he could deliver. You
-                                    won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then
-                                    we'll go with that data file! Hey, you add a one and two zeros to that or we walk!
-                                    You're going to do his laundry? I've got to find a way to escape.
+                                    {{ projeto.descricao }}
                                 </v-card-text>
                             </div>
                         </v-expand-transition>
@@ -49,7 +70,36 @@ export default {
     name: 'MeusProjetos',
     data() {
         return {
-
+            show: false,
+            projetos: [
+                {
+                    id: 1,
+                    titulo: 'Novidades em breve!',
+                    descricao: 'Novidades em breve!',
+                    imagem: 'https://www.faculdadesesi.edu.br/wp-content/uploads/2024/01/Em-breve.jpg',
+                    show: false,
+                    repositorio: 'https://github.com/AthosAlexandre/Meu-Portfolio',
+                    site: 'https://www.google.com.br'
+                },
+                {
+                    id: 2,
+                    titulo: 'Novidades em breve!',
+                    descricao: 'Novidades em breve!',
+                    imagem: 'https://www.faculdadesesi.edu.br/wp-content/uploads/2024/01/Em-breve.jpg',
+                    show: false,
+                    repositorio: 'https://github.com/AthosAlexandre/Meu-Portfolio',
+                    site: 'https://www.google.com.br'
+                },
+                {
+                    id: 3,
+                    titulo: 'Novidades em breve!',
+                    descricao: 'Novidades em breve!',
+                    imagem: 'https://www.faculdadesesi.edu.br/wp-content/uploads/2024/01/Em-breve.jpg',
+                    show: false,
+                    repositorio: 'https://github.com/AthosAlexandre/Meu-Portfolio',
+                    site: 'https://www.google.com.br'
+                }
+            ]
         }
     },
     components: {
@@ -58,4 +108,10 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+a{
+    color: white;
+    text-decoration: none;
+    display: inline-block;
+}
+</style>
