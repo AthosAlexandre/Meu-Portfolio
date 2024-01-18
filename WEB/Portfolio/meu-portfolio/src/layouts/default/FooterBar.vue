b1b945122c1c893f1280afc99fdd3a6275137f8e
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2024-01-18 15:56:25 -0400

    colocando link e to no footer

--- WEB/Portfolio/meu-portfolio/src/layouts/default/FooterBar.vue
+++ WEB/Portfolio/meu-portfolio/src/layouts/default/FooterBar.vue
@@ -1,15 +1,9 @@
 <template>
-    <v-footer class="bg-grey-darken-4" >
+  <v-footer class="bg-grey-darken-4">
     <v-row justify="center" no-gutters>
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        variant="text"
-        class="mx-2"
-        rounded="xl"
-      >
-        {{ link }}
+      <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-2" rounded="xl" target="_blanck"
+        :href="link.link" :to="link.to">
+        {{ link.nome }}
       </v-btn>
       <v-col class="text-center mt-4" cols="12">
         <strong>Athos Alexandre</strong> - <v-icon>mdi-copyright</v-icon>
@@ -20,26 +14,38 @@
 
 <script>
 export default {
-    name: 'FooterBar',
-    data() {
-        return {
-        links: [
-        'Home',
-        'Teste',
-        'Repositorio',
-        'Linkedin'
-      ]
+  name: 'FooterBar',
+  data() {
+    return {
+      links: [
+        {
+          nome: 'Home',
+          to: '/'
+        },
+        {
+          nome: 'Teste',
+          to: '/RotaTeste'
+        },
+        {
+          nome: 'Repositorio',
+          link: 'https://github.com/AthosAlexandre/Meu-Portfolio/tree/main'
+        },
+        {
+          nome: 'Linkedin',
+          link: 'https://www.linkedin.com/in/athos-alexandre-109199218/'
         }
-    },
-    
+      ]
+    }
+  },
+
 }
 </script>
 
 <style scoped>
-    .texto{
-        background-color: #000;
-        color: #fff;
-        padding: 20px;
-        
-    }
+.texto {
+  background-color: #000;
+  color: #fff;
+  padding: 20px;
+
+}
 </style>
