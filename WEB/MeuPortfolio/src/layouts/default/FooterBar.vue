d8c58736733840b8bcb6b84a787d4db44d174085
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2024-02-03 18:30:53 -0400

    Colocando meu site portfolio no meu portfolio

---
+++ WEB/MeuPortfolio/src/layouts/default/FooterBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-footer class="bg-grey-darken-4">
+    <v-row justify="center" no-gutters>
+      <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-2" rounded="xl" target="_blanck"
+        :href="link.link" :to="link.to">
+        {{ link.nome }}
+      </v-btn>
+      <v-col class="text-center mt-4" cols="12">
+        <strong>Athos Alexandre</strong> - <v-icon>mdi-copyright</v-icon>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterBar',
+  data() {
+    return {
+      links: [
+        {
+          nome: 'Home',
+          to: '/'
+        },
+        {
+          nome: 'Repositorio',
+          link: 'https://github.com/AthosAlexandre/Meu-Portfolio/tree/main'
+        },
+        {
+          nome: 'Linkedin',
+          link: 'https://www.linkedin.com/in/athos-alexandre-109199218/'
+        }
+      ]
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.texto {
+  background-color: #000;
+  color: #fff;
+  padding: 20px;
+
+}
+</style>
---
+++ WEB/MeuPortfolio/src/layouts/default/View.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-main>
+    <router-view />
+    <FooterBar/>
+  </v-main>
+</template>
+
+<script >
+import FooterBar from './FooterBar.vue';
+
+export default {
+  name: 'DefaultView',
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    FooterBar
+  },
+}
+
+  //
+</script>
